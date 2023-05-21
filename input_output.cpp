19ea961c08302444b0d93919e9ea8b3097e3043c
kiparvez <ndp943296@gmail.com>
2023-05-21 14:28:25 +0600

    first commit

---
+++ input_output.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    std::cout<<"Hello world";
+    std::cout<<"Hello "<<"World";
+    std::cout<<"Hello "<<std::endl<<"World"; 
+
+    int a,b;
+    cin>>a,b;
+    cout<<a<<b;
+}
---
+++ string_input.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char s[100];
+    cin>>s;
+    cout<<s;
+}
